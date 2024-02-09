2bf5a75c9b6ae2005f08272b48bbd73986382e14
LucasGabryellll <gabryelmonteiro1@gmail.com>
2024-02-09 10:41:48 -0300

    configurando frontend

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/JOGO-IHM/tree/main/frontend",
+  base: "/jogo_ihm_deploy",
 })
